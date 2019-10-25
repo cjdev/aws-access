278a56d27457b4bf5e71a255fb0e157124d4a971
Edward Langley <elangley@cj.com>
2019-10-25 15:06:58 -0700

    Add new AWS arn generator

--- src/domain.lisp
+++ src/domain.lisp
@@ -15,6 +15,36 @@
   (format nil "arn:aws:iam::~a:role/cjorganization/~a"
           account
           role))
+(defclass cj-organization-role ()
+  ((account :initarg :account
+            :reader account
+            :initform (error "must pass an account"))))
+(fw.lu:defclass+ cj-developer-role ((cj-organization-role (account)))
+  ())
+
+(defgeneric arn-for (type account resource)
+  (:method-combination list :most-specific-last)
+  (:documentation "get the arn for an aws resource")
+  (:method :around (-t account u)
+    (format nil "arn:aws:iam::~a:~a"
+            account
+            (serapeum:string-join (call-next-method)
+                                  "/")))
+  (:method list ((type (eql :mfa)) account user)
+    "mfa")
+  (:method list ((type (eql :mfa)) account (user string))
+    user)
+  (:method list ((type (eql :role)) account role)
+    "role")
+  (:method list ((type (eql :role)) account (role cj-organization-role))
+    "cjorganization")
+  (:method list ((type (eql :role)) account (role cj-developer-role))
+    "CJDeveloperAccessRole")
+  (:method list ((type (eql :role)) account (role string))
+    role))
+
+(defun cj-organization-role-arn (role)
+  (arn-for :role (account role) role))
 
 (defun read-new-mfa-token ()
   (format *query-io* "~&New MFA token: ")
